4dc0f078d7117f7d029d8636fa93dc1d464627b8
Jeff Xie <xiehuan09@gmail.com>
2022-06-14 23:44:43 +0800

    tracing: Set set_ftrace_filter with functions from file
    Signed-off-by: Jeff Xie <xiehuan09@gmail.com>

---
+++ ftrace-filter-file-funcs.sh
@@ -0,0 +1,11 @@
+if [ $# != 1 ];then
+	echo "args error. using:$0 filename"
+	exit 1;
+fi
+TRACE_PATH=/sys/kernel/debug/tracing/
+TMP_PATH=/tmp/ftrace-filter-tmp
+global -f $1 | grep -iv define | awk '{print $1}' > $TMP_PATH 
+while read line; do echo $line; echo $line >> $TRACE_PATH/set_ftrace_filter ;done < $TMP_PATH
+rm $TMP_PATH
+echo "completed"
+
