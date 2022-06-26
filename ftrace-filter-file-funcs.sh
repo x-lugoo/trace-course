490256585d87340379a96e6c92427b10ea0aaebf
Jeff Xie <xiehuan09@gmail.com>
2022-06-26 12:50:55 +0800

    tracing: add * for each filter functions

--- ftrace-filter-file-funcs.sh
+++ ftrace-filter-file-funcs.sh
@@ -4,7 +4,7 @@ if [ $# != 1 ];then
 fi
 TRACE_PATH=/sys/kernel/debug/tracing/
 TMP_PATH=/tmp/ftrace-filter-tmp
-global -f $1 | grep -iv define | awk '{print $1}' > $TMP_PATH 
+global -f $1 | grep -iv define |awk '{printf "%s*\n",$1}' > $TMP_PATH 
 while read line; do echo $line; echo $line >> $TRACE_PATH/set_ftrace_filter ;done < $TMP_PATH
 rm $TMP_PATH
 echo "completed"
